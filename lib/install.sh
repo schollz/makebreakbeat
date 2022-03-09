5a4266456f76279050dbdc0b7fbe98c6e7befa2b
Zack Scholl <zack.scholl@gmail.com>
2022-03-09 00:10:59 +0000

    install portedplugins if not already

--- lib/install.sh
+++ lib/install.sh
@@ -20,3 +20,15 @@ then
 	echo "installing aubio"
 	cd /tmp/ && git clone https://github.com/aubio/aubio && cd aubio && sed -i 's/curl -so/curl -k -so/g' scripts/get_waf.sh && ./scripts/get_waf.sh && ./waf configure && ./waf build && sudo ./waf install && cd /tmp && rm -rf aubio
 fi
+
+FOLDER=/home/we/.local/share/SuperCollider/Extensions/
+FILE=/home/we/.local/share/SuperCollider/Extensions/PortedPlugins/AnalogTape_scsynth.so
+if [ -d "$FOLDER" ]; then 
+	echo "folder exists" > /dev/null
+   if [ -f "$FILE" ]; then 
+	   echo "ok" > /dev/null
+   else
+	   echo "installing PortedPlugins..."
+	   cd /tmp && wget https://github.com/schollz/tapedeck/releases/download/PortedPlugins/PortedPlugins.tar.gz && tar -xvzf PortedPlugins.tar.gz && rm PortedPlugins.tar.gz && sudo rsync -avrP PortedPlugins /home/we/.local/share/SuperCollider/Extensions/
+   fi
+fi
